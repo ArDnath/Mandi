99cec32b2d2aefb8cb0eba84fac5af99b2e6f429
ariyaman <ariyamandebnath.ad@gmail.com>
2025-11-11 02:00:26 +0530

    chore: Seting up project Necessities

---
+++ components/container.tsx
@@ -0,0 +1,14 @@
+import {cn} from "@/lib/utils";
+import React, { Children, ReactNode } from "react";
+
+interface ContainerProps {
+    children: ReactNode;
+    className?: string;
+}
+
+export const Container: React.FC<ContainerProps> = ({
+    children,
+    className,
+}) => {
+    return <div className={cn("max-w-7xl mx-auto px-4 md:py-8")}>{children}</div>
+};
