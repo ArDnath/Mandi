3d962634bbdf8f9cf7cfb7f4a7d1395ba4f683b3
ariyaman <ariyamandebnath.ad@gmail.com>
2025-12-05 03:53:57 +0530

    refactor:build-fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,14 @@
+import { PrismaClient } from '@prisma/client';
 
-import { PrismaClient } from "@prisma/client";
+let prisma: PrismaClient;
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
 export default prisma;
